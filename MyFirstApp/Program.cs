0d9f197bdb4c622274ff70f972d529810d5789cb
Robert Ranta <robert.jalmari.ranta@tktk.ee>
2024-04-01 23:25:52 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// This is a comment to my code
+
+
+// Rakendus küsib kasutajalt tema nime
+// Rakendus tervitab teda nimepidi
+
+
+Console.WriteLine("Enter Your name:"); // output
+
+// String - sõne
+string userName = Console.ReadLine(); // input
+
+Console.WriteLine("Hello, " + userName + "!"); //output
+
+// string interpolation
+Console.WriteLine($"Hello, {userName}!"); // output
